ee6dab812d4cd31061684c6c89d0d27cae9b7acf
emi <emii.povi@gmail.com>
2025-11-08 04:23:42 +0000

    changed color of cube and some code mvoe arounds

--- src/core/app.cpp
+++ src/core/app.cpp
@@ -30,12 +30,17 @@ Application::~Application() {
 }
 
 void Application::Run(){
-    SDL_Event e;
+    //Final exit code
+    int exitCode{ 0 };
+
     bool quit = false;
+    SDL_Event e;
+    SDL_zero(e);
 
-    // Define a rectangle
-    SDL_FRect greenSquare {270, 190, 100, 100};
+    // Define a rectangle - still keeping for now but remove soon, just for debug
+    SDL_FRect square {270, 190, 100, 100};
 
+    //Main loop
     while (!quit) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) {
@@ -46,8 +51,8 @@ void Application::Run(){
         SDL_SetRenderDrawColor(mainRenderer, 0, 0, 0, 255); // Set render draw color to black
         SDL_RenderClear(mainRenderer); // Clear the renderer
 
-        SDL_SetRenderDrawColor(mainRenderer, 0, 255, 192, 203); // Set render draw color to green
-        SDL_RenderFillRect(mainRenderer, &greenSquare); // Render the rectangle
+        SDL_SetRenderDrawColor(mainRenderer, 255, 192, 203, 255); // Set render draw color to green
+        SDL_RenderFillRect(mainRenderer, &square); // Render the rectangle
         
         SDL_RenderPresent(mainRenderer); // Render the screen
     }
