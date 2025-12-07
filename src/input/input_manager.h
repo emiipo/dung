7a987a011ba491b8a782c8eaf5f5a53152e44414
emi <emii.povi@gmail.com>
2025-12-07 23:59:10 +0000

    starting work on input bdingings

--- src/input/input_manager.h
+++ src/input/input_manager.h
@@ -3,6 +3,7 @@
 #include <functional>
 #include <unordered_map>
 #include <list>
+#include <string>
 #include "../events/event.h"
 
 // TODO: When events are remade make sure this follows as we cant remove as of now
@@ -12,6 +13,18 @@ public:
     void HandleInputEvent(SDL_Event& inputEvent);
     void AddKeyDownListener(SDL_Keycode key, std::function<void()> callback);
 
+    void CreateBinding(std::string name, SDL_Keycode key = SDLK_UNKNOWN);
+    void CreateBinding(std::string name, std::list<SDL_Keycode> keys);
+
 private:
-    std::unordered_map<SDL_Keycode, Event> keyDownEvents;
+    struct InputBinding{
+        std::string name;
+        std::list<SDL_Keycode> keys;
+        Event OnKeyDown;
+        Event OnKeyUp;
+    };
+
+    std::list<InputBinding> mInputBindings;
+    std::unordered_map<SDL_Keycode, std::list<InputBinding*>> mKeyToBindingMap;
+    std::unordered_map<std::string, std::list<InputBinding*>> mNameToBindingMap;
 };
