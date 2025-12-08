e6bd58e3a78cd7d19506419bf91f6653f07ca7ae
Emi <emii.povi@gmail.com>
2025-12-08 19:32:04 +0000

    finished seperation of game and engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <SDL3/SDL.h>
 #include <iostream>
 #include <exception>
-#include "core/app.h"
+#include "dung.h"
 
 int main(int argc, char* argv[]) {
     try {
-        Application app;
-        app.Run();
+        Dung dung;
+        dung.Run();
     } catch (std::exception &e) {
         std::cerr << e.what() << "\n";
         return 1;
